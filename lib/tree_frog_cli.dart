a1e0833be4feca1686848a9263ae665ea16006d7
Yczar <babalolagbogo@gmail.com>
2023-03-22 11:50:46 +0100

    chore: where it all began

---
+++ lib/tree_frog_cli.dart
@@ -0,0 +1,10 @@
+/// tree_frog_cli, My new Dart CLI package
+///
+/// ```sh
+/// # activate tree_frog_cli
+/// dart pub global activate tree_frog_cli
+///
+/// # see usage
+/// tree_frog_cli --help
+/// ```
+library tree_frog_cli;
